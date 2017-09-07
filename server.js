7ad1124f19561165367ae0ab655c24fcaaa5d701
Peng Wang <pennelyx@gmail.com>
2017-09-07 18:38:52 -0400

    need note

--- server.js
+++ server.js
@@ -49,6 +49,10 @@ app.get("/scrape", function(req, res) {
 			if(link[0] === "/") {
 				link = "http://www.nba.com".concat(link);
 			};
+			//some links end with "/", which cause issues when link to article title
+			if(link[link.length-1] === "/") {
+				link = link.substring(0, link.length - 1);
+			}
 			result.title = title;
 			result.link = link;
 			result.saved = false;
@@ -140,6 +144,56 @@ app.get("/articles/unsave/:id", function(req, res) {
 });
 
 
+// // Grab an article by it's ObjectId
+// app.get("/articles/note:id", function(req, res) {
+//   // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+//   Article.findOne({ "_id": req.params.id })
+//   // ..and populate all of the notes associated with it
+//   .populate("note")
+//   // now, execute our query
+//   .exec(function(error, doc) {
+//     // Log any errors
+//     if (error) {
+//       console.log(error);
+//     }
+//     // Otherwise, send the doc to the browser as a json object
+//     else {
+//       res.json(doc);
+//     }
+//   });
+// });
+
+
+
+// // Create a new note or replace an existing note
+// app.post("/articles/note/:id", function(req, res) {
+//   // Create a new note and pass the req.body to the entry
+//   var newNote = new Note(req.body);
+
+//   // And save the new note the db
+//   newNote.save(function(error, doc) {
+//     // Log any errors
+//     if (error) {
+//       console.log(error);
+//     }
+//     // Otherwise
+//     else {
+//       // Use the article id to find and update it's note
+//       Article.findOneAndUpdate({ "_id": req.params.id }, { "note": doc._id })
+//       // Execute the above query
+//       .exec(function(err, doc) {
+//         // Log any errors
+//         if (err) {
+//           console.log(err);
+//         }
+//         else {
+//           // Or send the document to the browser
+//           res.send(doc);
+//         }
+//       });
+//     }
+//   });
+// });
 
 
 
